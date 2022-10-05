4d7143c93aa68e222878c505e6aab00eeae0c57c
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-05 22:31:58 +0200

    dev: make one-line list of Keywords

--- src/Components/Keywords.js
+++ src/Components/Keywords.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
-const KeywordItem = styled.li``
+const KeywordListWrapper = styled.ul`
+    padding: 0;
+    list-style-type: none;
+    font-size: 1.25em;
+`
 
 const Keywords = ({list}) => (
-    <ul>
-        {list.map(keyword => (
-            <KeywordItem key={keyword}>
-                {keyword}
-            </KeywordItem>)
-        )}
-    </ul>
+    <KeywordListWrapper>
+        {list.join(', ')}
+    </KeywordListWrapper>
 )
 
 export default Keywords
