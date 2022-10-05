d97c7e30d93e6626aa1229b2e1a80391353a8571
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-05 16:31:09 +0200

    dev: ExperienceList displays 2 columns

--- src/Components/ExperienceList.js
+++ src/Components/ExperienceList.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components'
 
-const ExperienceList = styled.article``
+const ExperienceList = styled.article`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media (max-width: 600px) {
+        grid-template-columns: 100%;
+    }
+    column-gap: 1em;
+    row-gap: 1em;
+`
 
 export default ExperienceList
