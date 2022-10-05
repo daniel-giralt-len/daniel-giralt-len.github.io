737a3863d20901338926c558aa86988a59f109c4
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-05 16:08:38 +0200

    dev: create JobDescription component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Title from "./Components/Title";
+import JobDescription from "./Components/JobDescription";
 
 function App() {
   return (
     <main>
         <Title>Daniel Giralt Len</Title>
+        <JobDescription>Full stack web developer (+ data engineer and QA)</JobDescription>
     </main>
   );
 }
