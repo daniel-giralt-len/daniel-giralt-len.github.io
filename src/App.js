8d6e69983e732e8c8f8c189a360e220c95457931
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-05 16:12:27 +0200

    dev: create ExperienceList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Title from "./Components/Title";
 import JobDescription from "./Components/JobDescription";
+import ExperienceList from "./Components/ExperienceList";
 
 function App() {
   return (
     <main>
         <Title>Daniel Giralt Len</Title>
         <JobDescription>Full stack web developer (+ data engineer and QA)</JobDescription>
+        <ExperienceList>
+          <section>Frontend</section>
+          <section>Backend</section>
+          <section>Architecture (Deploy+Monitor)</section>
+          <section>Data Engineering</section>
+          <section>Soft skills</section>
+          <section>Video Games + Personal</section>
+          <section>QA + Others</section>
+        </ExperienceList>
     </main>
   );
 }
---
+++ src/Components/ExperienceList.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const ExperienceList = styled.article``
+
+export default ExperienceList
