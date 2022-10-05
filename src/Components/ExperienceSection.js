1739caa8458bc8673d68eeb7663daed9e5458c0c
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-05 18:00:13 +0200

    dev: display Timeline in ExperienceSection

--- src/Components/ExperienceSection.js
+++ src/Components/ExperienceSection.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
 
+import Timeline from './Timeline'
+
 const ExperienceSectionWrapper = styled.section``
 
 const ExperienceSection = ({
-    sectionTitle
+    sectionTitle,
+    summary,
+    keywords,
+    timeline,
 }) => (
     <ExperienceSectionWrapper>
         <h3>{sectionTitle}</h3>
+        {JSON.stringify(keywords)}
+        {summary && (<p>{summary}</p>)}
+        {timeline && (<Timeline timeline={timeline} />)}
     </ExperienceSectionWrapper>
 )
 
