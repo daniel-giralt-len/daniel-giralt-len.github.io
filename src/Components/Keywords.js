6c05161052835478dd3c038c4566f4028f6bd514
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-17 20:31:48 +0200

    dev: center experience keywords

--- src/Components/Keywords.js
+++ src/Components/Keywords.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
 
 const KeywordListWrapper = styled.ul`
+    display: flex;
+    justify-content: center;
+
+    margin: 0.5em 0;
     padding: 0;
+
     list-style-type: none;
     font-size: 1.25em;
 `
